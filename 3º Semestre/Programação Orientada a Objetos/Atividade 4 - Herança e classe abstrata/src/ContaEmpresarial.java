c15ae3f06e107ec2125d187e14fa0b07eac71f6e
gabrielvmsf <99260479+gabrielvmsf@users.noreply.github.com>
2022-06-05 20:43:27 -0300

    Update ContaEmpresarial.java

--- 3º Semestre/Programação Orientada a Objetos/Atividade 4 - Herança e classe abstrata/src/ContaEmpresarial.java
+++ 3º Semestre/Programação Orientada a Objetos/Atividade 4 - Herança e classe abstrata/src/ContaEmpresarial.java
@@ -1,8 +1,8 @@
 public class ContaEmpresarial extends ContaEspecial {
     private String cnpjTitular;
 
-    public ContaEmpresarial(String cnpjTitular, int nroConta, String nomeTitular, double valorChequeEspecial) {
+    public ContaEmpresarial(String cnpj, int nroConta, String nomeTitular, double valorChequeEspecial) {
         super(nroConta, nomeTitular, valorChequeEspecial);
-        this.cnpjTitular = cnpjTitular;
+        this.cnpjTitular = cnpj;
     }
 }
