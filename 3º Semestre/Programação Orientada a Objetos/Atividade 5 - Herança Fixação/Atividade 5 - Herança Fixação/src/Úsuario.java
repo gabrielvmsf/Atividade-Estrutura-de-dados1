22e6af39550143834af7680219a177b74541a460
gabrielvmsf <99260479+gabrielvmsf@users.noreply.github.com>
2022-06-11 14:35:38 -0300

    Atividade

---
+++ 3º Semestre/Programação Orientada a Objetos/Atividade 5 - Herança Fixação/Atividade 5 - Herança Fixação/src/Úsuario.java
@@ -0,0 +1,50 @@
+public abstract class Úsuario {
+    private String nome;
+    private String telefone;
+    private String email;
+    private String cpf;
+
+
+    public Úsuarios(String nome, String telefone, String email, String cpf) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.cpf = cpf;
+    }
+
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+
+    
+}
