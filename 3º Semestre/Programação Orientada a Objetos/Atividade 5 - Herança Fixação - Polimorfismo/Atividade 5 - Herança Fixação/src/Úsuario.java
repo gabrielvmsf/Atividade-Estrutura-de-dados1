e38851fdf732a1825bf1b0ac787c08c62cefdbbd
gabrielvmsf <99260479+gabrielvmsf@users.noreply.github.com>
2022-06-22 18:03:45 -0300

    Atividade

---
+++ 3º Semestre/Programação Orientada a Objetos/Atividade 5 - Herança Fixação - Polimorfismo/Atividade 5 - Herança Fixação/src/Úsuario.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+
+public abstract class Úsuario {
+    private String nome;
+    private String telefone;
+    private String email;
+    private String cpf;
+    private int qtdRenovações;
+    private ArrayList<Emprestimo> emprestimosRealizados = new ArrayList<Emprestimo>();
+
+    public Úsuario(String nome, String telefone, String email, String cpf) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.cpf = cpf;
+        this.qtdRenovações = 0;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getQtdRenovações() {
+        return this.qtdRenovações;
+    }
+
+    public void setQtdRenovações(int qtdRenovações) {
+        this.qtdRenovações = qtdRenovações;
+    }
+
+    public boolean realizarEmprestimo(Publicações publicação, Úsuario usuario) {
+        Emprestimo emprestimo = new Emprestimo(publicação, usuario);
+        if(emprestimo.isVerificarDisponibilidade()) emprestimosRealizados.add(emprestimo); //só vai adicionar o emprestimo caso tenha retornado como verdadeiro
+
+        return true;
+    }
+
+    public boolean realizarRenovação(Emprestimo emprestimo) {
+        if (emprestimosRealizados.contains(emprestimo)) {
+            Publicações publicação = emprestimo.getPublicação();
+            emprestimo.renovarEmprestimo(publicação);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean devolverEmprestimo(Emprestimo emprestimo) {
+        if (emprestimosRealizados.contains(emprestimo)) {
+            Úsuario usuario = emprestimo.getUsuario();
+            Publicações publicação = emprestimo.getPublicação();
+            emprestimo.devolverEmprestimo(publicação, usuario);
+            emprestimosRealizados.remove(emprestimo);
+            return true;
+        }
+        return false;
+    }
+}
---
+++ 3º Semestre/Programação Orientada a Objetos/Atividade 5 - Herança Fixação - Polimorfismo/Atividade 5 - Herança Fixação/src/ÚsuarioEspecial.java
@@ -0,0 +1,5 @@
+public class ÚsuarioEspecial extends Úsuario {
+    public ÚsuarioEspecial(String nome, String telefone, String email, String cpf) {
+        super(nome, telefone, email, cpf);
+    }
+}
