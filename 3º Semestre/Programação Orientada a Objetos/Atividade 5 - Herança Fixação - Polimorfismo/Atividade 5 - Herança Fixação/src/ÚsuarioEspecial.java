8dcf21d5782a5576fb1216ed5bf5b7ed402ff774
gabrielvmsf <99260479+gabrielvmsf@users.noreply.github.com>
2022-06-22 18:14:04 -0300

    Retirar Lixo

--- 3º Semestre/Programação Orientada a Objetos/Atividade 5 - Herança Fixação - Polimorfismo/Atividade 5 - Herança Fixação/src/ÚsuarioEspecial.java
+++
@@ -1,5 +0,0 @@
-public class ÚsuarioEspecial extends Úsuario {
-    public ÚsuarioEspecial(String nome, String telefone, String email, String cpf) {
-        super(nome, telefone, email, cpf);
-    }
-}
