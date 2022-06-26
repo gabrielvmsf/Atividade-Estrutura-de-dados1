c3ace53b1a41dbf58f41c8c5ce0701845c8d5a8e
gabrielvmsf <99260479+gabrielvmsf@users.noreply.github.com>
2022-06-26 20:25:00 -0300

    Reestruturando código - Prática para prova

---
+++ 3º Semestre/Programação Orientada a Objetos/Atividade 6 - Atividade refeita - Treinamento para prova/src/ExcedeuQtdException.java
@@ -0,0 +1,8 @@
+public class ExcedeuQtdException extends RuntimeException {
+    public ExcedeuQtdException() {
+        super();
+    }
+    public ExcedeuQtdException(String msg) {
+        super(msg);
+    }
+}
---
+++ 3º Semestre/Programação Orientada a Objetos/Atividade 6 - Atividade refeita - Treinamento para prova/src/PublicaçãoNãoDisponívelException.java
@@ -0,0 +1,8 @@
+public class PublicaçãoNãoDisponívelException extends RuntimeException {
+    public PublicaçãoNãoDisponívelException() {
+        super();
+    }
+    public PublicaçãoNãoDisponívelException(String msg) {
+        super(msg);
+    }
+}
